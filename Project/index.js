35c5e4708253c27afeded6ebd2ea7df2501c9d7c
raghuanand16 <raghu.anand16@gmail.com>
2022-07-07 22:56:51 +0530

    07072022'

--- Project/index.js
+++ Project/index.js
@@ -1,115 +1,73 @@
+function abc(){
+    let a = ["Orange","Vanilla","Pineapple","Apple"]
+    a.map(function (myValue){
+        console.log('The Fruit Name is '+ myValue)
+    })
+    let b = [
+        {"Name":"Raghav","Age":16},
+        {"Name":"Pasha","Age":32},
+        {"Name":"Sambha","Age":48}
+    ]
+    b.map(function(myValue){
+        console.log(myValue.Name + ' ' + myValue.Age)
+    })
+}
+
 function def(){
-    console.log("Mars")
+    let a = ["Orange","Vanilla","Pineapple","Apple"]
+    console.log(a.filter(function(myValue){
+        return myValue != 'Apple'
+    }))
+    console.log(a)
+
+    document.getElementById("myId").textContent = a.filter(function(myValue){
+        return myValue != 'Apple'
+        })
 }
 
-function abc(a){    //a gets the function def
-    a() // calling the function a = def
+function ghi(){
+    let a = ["Orange","Vanilla","Pineapple","Apple"]
+    console.log(a.reduce(function (temp,myValue){
+           //return temp + '' + myValue //Insertion Order  
+           return myValue + ' ' + temp //reverse order
+    })) 
 }
 
-//abc(def)    //Callback Function
+function jkl(){
+    let a = ["Orange","Vanilla","Pineapple","Apple"]
+    a.forEach(function(myValue){
+        console.log(myValue)
+    })
+}
 
+function mno(){
+    aaa = {
+        "userId": 1,
+        "id": 1,
+        "title": "quidem molestiae enim"
+      }
+    console.log(typeof(aaa))    //object
+    let bbb = JSON.stringify(aaa)  
+    console.log(typeof(bbb))    //string
+    let jsonParse = JSON.parse(bbb)
+   console.log(typeof(jsonParse))   //Object
+}
 
+function pqr(){
+    let a = "              India               "
+    console.log(a)
+    console.log(a.trim())
+}
 
-// class Laptop{
-//     constructor(){
-//         this.keys = 96
-//         this.size = "14 inch"
-//         this.company = "dell"
-//         this.ram = "16 GB"
-//     }
-//     myInputL(){
-//         console.log("Hey I am myInput Method from Class Laptop")
-//         console.log(this.ram)
-//     }
-//     myChargeL(){
-//         console.log("Hey I am myCharge Method from Class Laptop")
-//     }
-// }
-// class Tablet extends Laptop{
-//     constructor(){
-//         this.keys = 99
-//         this.size = "10 inch"
-//         this.company = "Apple"
-//         this.ram = "32 GB"
-//     }
-//     myChargeT(){
-//         console.log("Hey I am myCharge Method from Apple Tab")
-//     }
-// }
-// // function def(){
-// //     let myObject1 = new Tablet()
-// //     myObject1.myCharge()
-// //     myObject1.myInput()
-// //     console.log(myObject1.keys)
-// //     console.log(myObject1.size)
-// //     }
-
-// class Mobile extends Tablet{
-//     constructor(){
-//         super()
-//         // this.keys = 105
-//         this.size = "5.1 inch"
-//         this.company = "Samsung"
-//         this.ram = "4 GB"
-//     }
-//     myInputM(){
-//         console.log("Hey I am myInput Method from Class Mobile")
-//         console.log(this.ram)
-//     }
-//     myChargeM(){
-//         console.log("Hey I am myCharge Method from Class Mobile")
-//     }
-// }
-// function abc(){
-// let myObject = new Mobile()
-// myObject.myCharge()
-// myObject.myInput()
-// console.log(myObject.keys)
-// console.log(myObject.size)
-// }
-
-// class Laptop{
-//     constructor(a,b){
-//         this.keys = a
-//         this.size = b
-//         this.company = "dell"
-//         this.ram = "16 GB"
-//     }
-//     myInput(){
-//         console.log("Hey I am myInput Method from Class Laptop")
-//         console.log(this.ram)
-//     }
-//     myCharge(){
-//         console.log("Hey I am myCharge Method from Class Laptop")
-//     }
-// }
-// function abc(){
-//     let myObject = new Laptop(96,"14 inch")
-//     myObject.myCharge()
-//     myObject.myInput()
-//     console.log(myObject.keys)
-//     console.log(myObject.size)
-//     }
-
-
-
-// class Laptop{
-//     constructor(){
-//         this.keys = 96
-//         this.size = "14 inch"
-//         this.company = "dell"
-//         this.ram = "16 GB"
-//     }
-//     myInput(){
-//         console.log("Hey I am myInput Method")
-//     }
-//     myCharge(){
-//         console.log("Hey I am myCharge Method")
-//     }
-// }
-// function abc(){
-// let myObject = new Laptop()
-// myObject.myCharge()
-// myObject.myInput()
-// console.log(myObject.ram)
-// }
+function stu(){
+    let a = ["Orange","Vanilla","Pineapple","Apple"]
+    console.log(...a)
+    let b = [
+        {"Name":"Raghav","Age":16},
+        {"Name":"Pasha","Age":32},
+        {"Name":"Sambha","Age":48}
+    ]
+    console.log(...b)
+    let c = "India"
+    console.log(...c)
+}
