07e97c26567e39d4815ebcf7ff9a9e7e9cfc72e8
raghuanand16 <raghu.anand16@gmail.com>
2022-07-19 23:03:40 +0530

    19072022

--- Project/index.js
+++ Project/index.js
@@ -71,3 +71,75 @@ function stu(){
     let c = "India"
     console.log(...c)
 }
+
+function dom()
+{
+    console.log(document.getElementById("myId").innerHTML)  //Venus
+    console.log(document.getElementById("myId").innerText)  //venus
+    console.log(document.getElementById("myId").textContent)    //venus
+    console.log(document.getElementsByClassName("myClass")[0].innerHTML)   //Earth
+    console.log(document.getElementsByClassName("myClass")[0].innerText)   //Earth
+    console.log(document.getElementsByClassName("myClass")[0].textContent) //Earth
+    console.log(document.getElementsByTagName("h1")[0].innerHTML)  //Pluto
+    for(let i=0;i<document.getElementsByTagName("p").length;i++)
+    {
+        console.log(document.getElementsByTagName("p")[i].innerHTML)    //Mars, Jupitor, Earth
+    }
+    console.log(document.getElementsByTagName("p")[0].innerHTML)    //Mars
+    console.log(document.getElementsByTagName("p")[1].innerHTML)    //Jupitor
+    console.log(document.getElementsByTagName("p")[2].innerHTML)    //Earth
+    console.log(document.getElementsByName("hello")[0].innerHTML)   // venus
+
+    document.getElementById("myId").innerHTML = "Mercury"
+    console.log(document.getElementById("myId").innerHTML)
+
+    //1st Method
+    console.log(document.getElementsByName("firstName")[0].value)
+    console.log(document.getElementsByName("lastName")[0].value)
+
+    //2nd Method
+    console.log(document.forms["myForm"]["firstName"][0].value)
+    console.log(document.forms["myForm"]["firstName"][1].value)
+    console.log(document.forms["myForm"]["firstName"][2].value)
+    console.log(document.forms["myForm"]["lastName"].value)
+}
+
+function assignFunction(){
+    const formFieldsData = {
+        name: document.forms["assignForm"]["name"].value,
+        email: document.forms["assignForm"]["email"].value,
+        password: document.forms["assignForm"]["password"].value
+    }
+    console.log(formFieldsData)
+    let a = signUpFormValidation(formFieldsData)    //error Object
+    //a = error object = {
+      //  name:'Name should be atleast 4 chars and less than 20 chars' or ''
+    // , password: 'Password should be atleast 6 chars and less than 30 chars' or '', 
+    //email: 'Not a valid email' or ''}
+
+    document.getElementById("namelabel").innerHTML = a.name
+    document.getElementById("emaillabel").innerHTML = a.email
+    document.getElementById("passwordlabel").innerHTML = a.password
+}
+
+const signUpFormValidation = (formData) => {
+    const error = {}
+    const fields = ['email', 'password', 'name']
+    fields.forEach(field => {
+        if (!formData[`${field}`]) {
+            error[[field]] = `${field} not present`
+        }                    
+    })
+    if (formData.name && (formData.name.length < 3 || formData.name.length > 20)) {
+        error['name'] = 'Name should be atleast 4 chars and less than 20 chars'
+    }
+    if (formData.password && (formData.password.length < 6 || formData.password.length > 30)) {
+        error['password'] = 'Password should be atleast 6 chars and less than 30 chars'
+    }
+
+    if (formData.email && isEmailAddress(formData.email) === null) {
+        error['email'] = 'Not a valid email'
+    }
+    if (Object.keys(error).length === 0) return null
+    return error
+}
---
+++ nodeproject/index.js
@@ -0,0 +1,27 @@
+// import fs from 'fs'
+
+// fs.writeFileSync('Myfile.txt','Hey I am from Pluto')
+
+// fs.unlinkSync('Myfile.txt')
+
+// const os = require('os')
+// import os from 'os'
+// console.log(os.arch())      //system's arch i.e. 64 or 32
+// console.log(os.cpus())      //cpu information
+// console.log(os.freemem())   //ram free memory
+// console.log(os.platform())  //windows/mac/ubuntu
+// console.log(os.hostname())  
+// console.log(os.totalmem())  //ram total capacity
+//try other methods only
+
+
+// import process from 'process'
+
+// console.log(process.argv[1])
+
+// console.log(parseInt(process.argv[2])+ parseInt(process.argv[3]))
+
+// for(let i=0;i<10;i+=2){
+//     console.log(i)
+// }
+//0 2 4 6 8 
